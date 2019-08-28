23916edc33ac3a55824dbbb02ab452b793a4f232
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 22:03:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,9 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_hash = {
+    :id => rand(1000)
+  }
 end
 
 def my_hash_creator(key, value)
