9a199631a07fa9f823fc92f0ab05f2be1b7d44c7
Austin Smith <trunk9slamchest@gmail.com>
2019-08-28 22:18:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,10 +37,17 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   
   # hash.default = 1
-if hash[key] ? 
+if hash[key]
   hash[key] += 1
 else
   hash[key] = 1
+end
+
+if hash[key] ?
+  hash[key] += 1
+else
+  hash[key] = 1
+end
 
 end
 
